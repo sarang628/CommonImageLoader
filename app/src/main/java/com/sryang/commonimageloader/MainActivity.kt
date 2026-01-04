f3d7b653d3804b960bafdecc684bce94f30ebdac
sryang <sarang628@gmail.com>
2026-01-04 19:20:19 +0900

    change SubcomposeAsyncImage in TorangAsyncImage

--- app/src/main/java/com/sryang/commonimageloader/MainActivity.kt
+++ app/src/main/java/com/sryang/commonimageloader/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.sryang.commonimageloader
 
+import TorangAsyncImage
 import ZoomableTorangAsyncImage
 import android.os.Bundle
 import android.util.Log
@@ -7,14 +8,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
@@ -22,12 +33,12 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.sarang.torang.di.image.ImageLoadData
-import com.sarang.torang.di.image.ZoomableTorangAsyncImage
-import com.sarang.torang.di.image.provideZoomableTorangAsyncImage
+import com.sarang.torang.di.image.ProvideZoomableTorangAsyncImage
 import com.sarang.torang.di.pinchzoom.PinchZoomImageBox
 import com.sarang.torang.di.pinchzoom.isZooming
 import com.sarang.torang.di.pinchzoom.rememberPichZoomState
 import com.sryang.commonimageloader.ui.theme.CommonImageLoaderTheme
+import coil.compose.AsyncImage
 
 
 class MainActivity : ComponentActivity() {
@@ -42,13 +53,16 @@ class MainActivity : ComponentActivity() {
                 ) {
                     NavHost(
                         navController = rememberNavController(),
-                        startDestination = "PinchZoomImageBoxTest"
+                        startDestination = "TorangAsyncImageTest"
                     ) {
                         composable("ZoomableTorangAsyncImageTest") {
                             ZoomableTorangAsyncImageTest()
                         }
                         composable("PinchZoomImageBoxTest") {
                             PinchZoomImageBoxTest()
+                        }
+                        composable("TorangAsyncImageTest"){
+                            TorangAsyncImageTest()
                         }
                     }
 
@@ -59,14 +73,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun TorangAsyncImageTest(){
+    var input by remember { mutableStateOf("http://sarang628.iptime.org:89/restaurant_images/311/2025-12-24/01_07_57_132.jpg") }
+    var url  by remember { mutableStateOf("") }
+    Box(Modifier.fillMaxSize()){
+        Column {
+            TorangAsyncImage(modifier = Modifier.size(200.dp),
+                             model = url)
+            AsyncImage(modifier = Modifier.size(200.dp),
+                model = url,
+                contentDescription = null)
+        }
+
+        Column(modifier = Modifier.fillMaxWidth()
+                                  .align(Alignment.BottomCenter)) {
+            TextField(value = input,
+                      onValueChange = { input = it })
+            Button(onClick = { url = input }) {
+                Text("Load")
+            }
+        }
+
+    }
+
+
+
+}
+
+@Composable
 fun ZoomableTorangAsyncImageTest() {
     LazyColumn {
         items(10) {
-            ZoomableTorangAsyncImage("https://artrkl.com/cdn/shop/articles/thecreationofadam-1690035964350_d2d6280f-ed1d-465e-ad42-0ea0bbbcefde.webp?v=1690563054&width=1100")
-            ZoomableTorangAsyncImage("https://artrkl.com/cdn/shop/articles/thecreationofadam-1690035964350_d2d6280f-ed1d-465e-ad42-0ea0bbbcefde.webp?v=1690563054&width=1100")
-            ZoomableTorangAsyncImage("https://artrkl.com/cdn/shop/articles/thecreationofadam-1690035964350_d2d6280f-ed1d-465e-ad42-0ea0bbbcefde.webp?v=1690563054&width=1100")
-            ZoomableTorangAsyncImage("https://artrkl.com/cdn/shop/articles/thecreationofadam-1690035964350_d2d6280f-ed1d-465e-ad42-0ea0bbbcefde.webp?v=1690563054&width=1100")
-            ZoomableTorangAsyncImage("https://artrkl.com/cdn/shop/articles/thecreationofadam-1690035964350_d2d6280f-ed1d-465e-ad42-0ea0bbbcefde.webp?v=1690563054&width=1100")
             ZoomableTorangAsyncImage("https://artrkl.com/cdn/shop/articles/thecreationofadam-1690035964350_d2d6280f-ed1d-465e-ad42-0ea0bbbcefde.webp?v=1690563054&width=1100")
         }
     }
@@ -74,37 +111,26 @@ fun ZoomableTorangAsyncImageTest() {
 
 @Composable
 fun PinchZoomImageBoxTest() {
-    val zoomState = rememberPichZoomState()
+    var zoomState = rememberPichZoomState()
 
     LaunchedEffect(zoomState) {
-        snapshotFlow { zoomState.isZooming }.collect {
+        snapshotFlow { zoomState.value.isZooming }.collect {
             Log.d("__PinchZoomImageBoxTest", "isZooming : $zoomState.isZooming")
         }
     }
 
-    PinchZoomImageBox(
-        imageLoader = { modifier, url, contentScale ->
-            provideZoomableTorangAsyncImage().invoke(
-                ImageLoadData(
-                    modifier = modifier,
-                    url = url,
-                    contentScale = contentScale ?: ContentScale.Fit
-                )
-            )
-        },
-        zoomState = zoomState
-    ) { imageLoader ->
-        Log.d("__PinchZoomImageBoxTest", "imageLoader : $imageLoader")
+    PinchZoomImageBox(zoomState){
         LazyColumn {
             items(100) {
                 Box {
-                    imageLoader.invoke(
-                        Modifier,
-                        "https://artrkl.com/cdn/shop/articles/thecreationofadam-1690035964350_d2d6280f-ed1d-465e-ad42-0ea0bbbcefde.webp?v=1690563054&width=1100",
-                        ContentScale.Crop,
-                        100.dp
+                    ProvideZoomableTorangAsyncImage(
+                        onPress = { zoomState.value = it.copy(url = "https://artrkl.com/cdn/shop/articles/thecreationofadam-1690035964350_d2d6280f-ed1d-465e-ad42-0ea0bbbcefde.webp?v=1690563054&width=1100") },
+                        data = ImageLoadData(
+                            modifier = Modifier,
+                            url = "https://artrkl.com/cdn/shop/articles/thecreationofadam-1690035964350_d2d6280f-ed1d-465e-ad42-0ea0bbbcefde.webp?v=1690563054&width=1100",
+                            contentScale = ContentScale.Fit
+                        )
                     )
-                    //Text(text = if (zoomState.isZooming) "isZooming" else "")
                 }
             }
         }
--- library/src/main/java/TorangAsyncImage.kt
+++ library/src/main/java/TorangAsyncImage.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
@@ -22,52 +23,68 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import coil.compose.SubcomposeAsyncImage
 import coil.request.ImageRequest
 import com.sryang.library.R
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+
+enum class ImageState {
+    LOADING,
+    SUCCESS,
+    FAILED
+}
 
 /**
  * @param model Either an [ImageRequest] or the [ImageRequest.data] value.
  */
 @Composable
 fun TorangAsyncImage(
-    model: Any?,
-    modifier: Modifier = Modifier,
-    progressSize: Dp = 50.dp,
-    errorIconSize: Dp = 50.dp,
-    contentScale: ContentScale = ContentScale.Fit,
-    contentDescription : String = "",
-    @DrawableRes previewPlaceHolder: Int? = null,
+    model                           : Any?          = "",
+    modifier                        : Modifier      = Modifier,
+    progressSize                    : Dp            = 50.dp,
+    errorIconSize                   : Dp            = 50.dp,
+    contentScale                    : ContentScale  = ContentScale.Fit,
+    contentDescription              : String        = "",
+    @DrawableRes previewPlaceHolder : Int?          = null,
 ) {
-    var state by remember { mutableIntStateOf(0) }
-    val coroutine = rememberCoroutineScope()
-    LaunchedEffect(key1 = model) { state = 0 }
     Box(modifier = modifier, contentAlignment = Alignment.Center) {
         if (LocalInspectionMode.current) {
-            Image(modifier = Modifier.align(Alignment.Center).size(progressSize),
+            Image(modifier = Modifier.align(Alignment.Center)
+                                     .size(progressSize),
                 painter = painterResource(id = previewPlaceHolder ?: R.drawable.ic_loading),
                 contentDescription = contentDescription,
             )
-        } else if (state == 0 || state == 1) {
-            AsyncImage(modifier = Modifier.fillMaxSize(),
-                model = ImageRequest.Builder(LocalContext.current).data(model).build(),
-                onSuccess = { coroutine.launch { delay(10); state = 1  /*success*/ } },
-                onError = { coroutine.launch { delay(10); state = 2 /*failed*/ } },
+        } else
+        {
+            SubcomposeAsyncImage(
+                modifier = Modifier.fillMaxSize(),
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(model)
+                    .crossfade(300)
+                    .build(),
+                contentDescription = null,
                 contentScale = contentScale,
-                contentDescription = contentDescription
-            )
-        } else if (state == 2) {
-            AsyncImage(modifier = Modifier.size(errorIconSize).align(Alignment.Center),
-                model = ImageRequest.Builder(LocalContext.current).data(R.drawable.ic_connection_error).crossfade(true).build(),
-                contentDescription = contentDescription,
-            )
-        }
-        if (state == 0) {
-            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center).size(progressSize),
-                strokeWidth = 2.dp,
-                strokeCap = StrokeCap.Round,
-                color = Color.LightGray
+                loading = {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        CircularProgressIndicator(
+                            strokeWidth = 2.dp,
+                            modifier = Modifier.size(progressSize)
+                        )
+                    }
+                },
+                error = {
+                    Box(Modifier.fillMaxSize()){
+                        Image(
+                            modifier = Modifier.size(errorIconSize).align(Alignment.Center),
+                            painter = painterResource(R.drawable.ic_connection_error),
+                            contentDescription = null
+                        )
+                    }
+                }
             )
         }
     }
