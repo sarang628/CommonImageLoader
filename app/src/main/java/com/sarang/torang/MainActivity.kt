af9332d7d67ac1e3d9d920741e1102e3634b6612
sryang <sarang628@gmail.com>
2026-01-22 10:09:04 +0900

    add feed image for test

--- app/src/main/java/com/sarang/torang/MainActivity.kt
+++ app/src/main/java/com/sarang/torang/MainActivity.kt
@@ -10,14 +10,18 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -28,6 +32,7 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.sarang.torang.data.ReviewAndImage
 import com.sarang.torang.ui.theme.CommonImageLoaderTheme
 import com.sarang.torang.repository.feed.FeedFlowRepository
 import com.sarang.torang.repository.feed.FeedLoadRepository
@@ -42,37 +47,58 @@ class MainActivity : ComponentActivity() {
 
     @Inject lateinit var feedLoadRepository: FeedLoadRepository
     @Inject lateinit var feedRepository: FeedRepository
-    @Inject lateinit var feedFlowRepotiroy: FeedFlowRepository
+    @Inject lateinit var feedFlowRepository: FeedFlowRepository
 
     @OptIn(ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navHostController = rememberNavController()
+            LaunchedEffect(Unit) {
+                feedLoadRepository.setLoadTrigger(true)
+            }
             CommonImageLoaderTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    NavHost(
-                        navController = rememberNavController(),
-                        startDestination = "TorangAsyncImageTest"
-                    ) {
-                        composable("ZoomableTorangAsyncImageTest") {
-                            ZoomableTorangAsyncImageTest()
-                        }
-                        composable("PinchZoomImageBoxTest") {
-                            PinchZoomImageBoxTest()
-                        }
-                        composable("TorangAsyncImageTest"){
-                            TorangAsyncImageTest()
-                        }
-                        composable("FeedRepositoryTest"){
-                            FeedRepositoryTestScreen(feedRepository = feedRepository,
-                                                     feedLoadRepository = feedLoadRepository,
-                                                     feedFlowRepository = feedFlowRepotiroy)
+                    Scaffold() {
+                        Box(Modifier.padding(it)){
+                            NavHost(
+                                navController = navHostController,
+                                startDestination = "menu"
+                            ) {
+                                composable("menu"){
+                                    Column {
+                                        Button({
+                                            navHostController.navigate("TorangAsyncImageTest")
+                                        }) {
+                                            Text("TorangAsyncImageTest")
+                                        }
+                                        Button({
+                                            navHostController.navigate("FeedRepositoryTest")
+                                        }) {
+                                            Text("FeedRepositoryTest")
+                                        }
+                                    }
+                                }
+                                composable("ZoomableTorangAsyncImageTest") {
+                                    ZoomableTorangAsyncImageTest()
+                                }
+                                composable("PinchZoomImageBoxTest") {
+                                    PinchZoomImageBoxTest()
+                                }
+                                composable("TorangAsyncImageTest"){
+                                    TorangAsyncImageTest()
+                                }
+                                composable("FeedRepositoryTest"){
+                                    FeedRepositoryTestScreen(feedRepository = feedRepository,
+                                        feedLoadRepository = feedLoadRepository,
+                                        feedFlowRepository = feedFlowRepository)
+                                }
+                            }
                         }
                     }
-
                 }
             }
         }
@@ -82,23 +108,36 @@ class MainActivity : ComponentActivity() {
     fun TorangAsyncImageTest(){
         var input by remember { mutableStateOf("http://sarang628.iptime.org:89/restaurant_images/311/2025-12-24/01_07_57_132.jpg") }
         var url  by remember { mutableStateOf("") }
+        var feeds : List<ReviewAndImage>? by remember { mutableStateOf(listOf()) }
+        LaunchedEffect(Unit) {
+            feedLoadRepository.feeds.collect {
+                feeds = it
+            }
+        }
+
         Box(Modifier.fillMaxSize()){
             LazyColumn {
-                items(100){
-                    TorangAsyncImage(modifier = Modifier.size(200.dp),
-                        model = url)
+                item{
+                    Column(modifier = Modifier.fillMaxWidth()
+                        .align(Alignment.BottomCenter)) {
+                        TextField(value = input,
+                            onValueChange = { input = it })
+                        Button(onClick = { url = input }) {
+                            Text("Load")
+                        }
+                    }
+                }
+                item{
+                    TorangAsyncImage(modifier   = Modifier.size(200.dp),
+                                     model      = url)
+                }
+                feeds?.let {
+                    items(it.flatMap { it.images }){
+                        TorangAsyncImage(modifier   = Modifier.size(200.dp),
+                            model      = BuildConfig.REVIEW_IMAGE_SERVER_URL + it.pictureUrl)
+                    }
                 }
             }
-
-            Column(modifier = Modifier.fillMaxWidth()
-                .align(Alignment.BottomCenter)) {
-                TextField(value = input,
-                    onValueChange = { input = it })
-                Button(onClick = { url = input }) {
-                    Text("Load")
-                }
-            }
-
         }
     }
 }
